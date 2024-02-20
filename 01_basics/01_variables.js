3b6cf0774d473bcc2956c0db57eddeec90c53c53
SatyaDEVILs <159985246+SatyaDEVILs@users.noreply.github.com>
2024-02-20 10:17:13 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="satyajit.rath2001@gmail.com"
+var accountPassword="12345"
+accountCity="Bhubaneswar"
+let accountState;
+
+
+//accountId=2
+accountEmail="hc@hc.com"
+accountPassword="324151"
+accountCity="Bengaluru"
+console.log(accountId);
+console.log(accountPassword)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
