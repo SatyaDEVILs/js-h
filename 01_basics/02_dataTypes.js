cce3d3deecfb5ffb842b3100070a12086dc5aaf6
SatyaDEVILs <159985246+SatyaDEVILs@users.noreply.github.com>
2024-02-20 10:42:42 +0000

    Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3)//we are using Node.js and not browser
+console.log(3+3);//Ensure code readability
+console.log("Hitesh")
+let name="satyajit"
+let age=23
+let isLoggedIn=false
+
+//number=> range 2^53
+//bigint=> range>2^53
+//string=>
+//boolean=>true/false
+//null=>standalone value
+//undefined=> value has not be assigned
+//symbol=>unique
+
+
+//object
+console.log(typeof "Satyajit Rath");
+console.log(typeof null);
+console.log(typeof undefined)
