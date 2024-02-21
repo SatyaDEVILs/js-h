9731ad35edd1a3069acf550a875f3174d018eb75
SatyaDEVILs <159985246+SatyaDEVILs@users.noreply.github.com>
2024-02-21 06:13:56 +0000

    CoversioN of Datatypes indepth

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,27 @@ let num=33
 let str=String(num)
 console.log(str);
 console.log(typeof str);
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+
+let str1="hello"
+let str2="involead"
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2);
+
+// console.log(+true);
+// console.log(+"")
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter);
